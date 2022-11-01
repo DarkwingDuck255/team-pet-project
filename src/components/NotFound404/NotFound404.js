d1f7aabe7929a8ceb12af23093a212be60ad23ca
gog3d <gluhovog3d@gmail.com>
2022-11-01 14:49:45 -0400

    add pad new design

--- src/components/NotFound404/NotFound404.js
+++ src/components/NotFound404/NotFound404.js
@@ -7,7 +7,7 @@ import notFoundImg from '../../images/Page Not Found 4 by Streamlinehq.png';
 const NotFound404 = () => {
   return (
     <div className='notFound404'>
-      <img src={notFoundImg} className='notFound404-img'  alt="404 image"/>
+      <img src={notFoundImg} className='notFound404__img'  alt="404 image"/>
       <h1 className='notFound404__title'>Кто-то украл эту страницу!</h1>
       <p className='notFound404__text'>Мы в этом разберемся!</p>
       <p className='notFound404__text'>А пока посмотрите на нашу замечательную команду ;)</p>
