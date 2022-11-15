c043d3820f1f65e277867bd8c768565f6ab8de57
Арсен Абгарян <83244299+DarkwingDuck255@users.noreply.github.com>
2022-11-15 23:44:49 +0300

    Revert "About us page"

--- src/components/AboutSlogan/AboutSlogan.js
+++ src/components/AboutSlogan/AboutSlogan.js
@@ -8,15 +8,15 @@ const AboutSlogan = () => {
           <h1 className='about_slogan__title-1'>
             We can do everything!
           </h1>
-          <div className='about_slogan__title-1 about_slogan__title-1-shadow'>
+          <h1 className='about_slogan__title-1-shadow'>
             We can do everything!
-         </div>
+         </h1>
         </div>
         <div className='about_slogan__text-container'>
           <div className='about_slogan__text'>
             <p className='about_slogan__text-p'>Мы молодые, амбициозные ребята и нам нравится наше дело</p>
           </div>
-          <div className='about_slogan__text about_slogan__text-right'>
+          <div className='about_slogan__text'>
             <p className='about_slogan__text-p'>В процессе поиска работы мы решили объединиться и показать на деле, что мы умеем</p>
           </div>
         </div>
--- src/components/PersonalList/PersonalList.js
+++ src/components/PersonalList/PersonalList.js
@@ -20,18 +20,16 @@ const PersonalList = () => {
           data.team_members.map((item, index) => {
             return (
               <div key={uuidv4()} >
-                <PersonalListCard item={item} />
+                <PersonalListCard item={item}/>
               </div>
             )
           })
         }
       </div>
-      {
-          <div className='list__connection'>
-            <span className='list__connection-text-1'>Связь с командой</span>
-            <span className='list__connection-text-2'>- wcde-studio@yandex.ru </span>
-          </div>
-      }
+      <div className='list__connection'>
+        <span className='list__connection-text-1'>Связь с командой</span>
+        <span className='list__connection-text-2'>- wcde-studio@yandex.ru </span>
+      </div>
     </div>
   );
 }
